61cba206b99d4dfe266297522c3825c5574b4ea2
windyrain <windyrain1994@163.com>
2023-06-16 13:47:30 +0800

    perf: 优化弹窗动效

--- assets/Script/Popup.ts
+++ assets/Script/Popup.ts
@@ -2,7 +2,7 @@ const { ccclass } = cc._decorator;
 
 @ccclass
 export default class Popup extends cc.Component {
-  onLoad() {
+  protected onEnable(): void {
     this.node.opacity = 0;
     cc.tween(this.node).to(0.3, { opacity: 255 }).start();
   }
